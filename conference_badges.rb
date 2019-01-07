876fbc32dbfa371452ef55cb75ca0ba6940243dc
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-01-07 13:16:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+
+def conferennce_badges(badge_maker)
+batch_badge_creator=["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+batch_badge_creator.each do |name|
+  puts "Hello, my name is #{name}."
+end
